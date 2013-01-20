fc64d1392484d86b848e5fc4cd7d17e548f528d8
ftiasch <ftiasch0@gmail.com>
2013-01-20 19:44:42 +0800

    change redirect path

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -37,7 +37,7 @@ class SubmissionsController < ApplicationController
 
     respond_to do |format|
       if @submission.save
-        format.html { redirect_to @submission, notice: 'Submission was successfully created.' }
+        format.html { redirect_to :submissions, notice: 'Submission was successfully created.' }
         format.json { render json: @submission, status: :created, location: @submission }
       else
         format.html { render action: "new" }
